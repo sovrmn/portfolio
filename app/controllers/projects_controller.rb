277425b34d45fcd438a0421bb6e2097a7db161dd
Sovana Romano <sovana_srinn@hotmail.com>
2021-01-04 13:11:03 +0100

    add controller projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,41 @@
+class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to project_path(@project)
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :url, :type, :technical_skills)
+  end
+end
